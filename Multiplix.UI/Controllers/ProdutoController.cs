e9cba4b1a05610d7ea959f7164cf93a0512c9ada
esdrasifrn <esdras.valentim@yahoo.com.br>
2019-10-07 17:10:22 -0300

    cadastro de compras

--- Multiplix.UI/Controllers/ProdutoController.cs
+++ Multiplix.UI/Controllers/ProdutoController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Multiplix.Domain.DTOs;
 using Multiplix.Domain.Entities;
 using Multiplix.Domain.Interfaces.Services;
+using Multiplix.UI.Models;
 
 namespace Multiplix.UI.Controllers
 {
@@ -21,6 +23,58 @@ namespace Multiplix.UI.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult IndexProduto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdicionarProduto()
+        {
+            ViewData["Title"] = "Novo Produto";
+            ProdutoDTO produtoDTO = new ProdutoDTO();
+            return View("AdicionarEditarProduto", produtoDTO);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarProduto(ProdutoDTO produtoDTO)
+        {
+            ViewData["Title"] = "Novo Produto";
+            return SalvarProduto(produtoDTO, "Produdo adicionado com sucesso!");
+        }
+
+        [HttpGet]
+        public IActionResult EditarProduto(int produtoId)
+        {
+            ViewData["Title"] = "Editar Produto";
+            Produto produto = _serviceProduto.ObterPorId(produtoId);
+            ProdutoDTO produtoDTO = new ProdutoDTO()
+            {
+                Descricao = produto.Descricao               
+            };
+
+            return View("AdicionarEditarProduto", produtoDTO);
+        }
+
+        [HttpPost]
+        public IActionResult EditarProduto(ProdutoDTO produtoDTO)
+        {
+            ViewData["Title"] = "Editar produto";
+            return SalvarProduto(produtoDTO, "Produto alterado com sucesso!");
+        }
+
+        private IActionResult SalvarProduto(ProdutoDTO  produtoDTO, string mensagemRetorno)
+        {
+            var result = _serviceProduto.Salvar(produtoDTO);
+
+            if (result.IsValid)
+            {
+                TempData["success"] = mensagemRetorno;
+                return RedirectToAction("IndexProduto");
+            }
+            return View("AdicionarEditarProduto", produtoDTO);
         }
 
         public JsonResult PesquisaProduto(string searchTerm, int pageNumber, int ParceiroId)
@@ -76,6 +130,82 @@ namespace Multiplix.UI.Controllers
                 pageSize,
                 results,
                 totalResults
+            });
+        }
+
+
+        [HttpPost]
+        public JsonResult ListaProdutos(DataTableAjaxPostModel dataTableModel)
+        {
+            /*
+             * consumido por um DataTable serverSide processing ajax POST
+             * 
+             * o código deste controlador pode ser usado como base para futuras implementações genéricas com DataTable
+             */
+
+            string searchTerm = dataTableModel.search.value;
+            string firstOrderColumnIdx = dataTableModel.order.Count > 0 ? dataTableModel.order[0].column.ToString() : "";
+            string firstOrderDirection = dataTableModel.order.Count > 0 ? dataTableModel.order[0].dir.ToString() : "";
+
+            IEnumerable<Produto> produtos = new List<Produto>();
+
+            if (!String.IsNullOrEmpty(dataTableModel.search.value))
+            {
+                produtos = _serviceProduto.Buscar(
+                    x => x.Descricao.Contains(searchTerm) 
+                );
+            }
+            else
+                produtos = _serviceProduto.ObterTodos();
+
+            if (firstOrderColumnIdx.Length > 0)
+            {
+                Func<Produto, Object> orderByExpr = null;
+
+                switch (firstOrderColumnIdx)
+                {
+                    case "1":
+                        orderByExpr = x => x.Descricao;
+                        break;                    
+                }
+
+                if (orderByExpr != null)
+                {
+                    if (firstOrderDirection.Length > 0 && firstOrderDirection.Equals("desc"))
+                        produtos = produtos.OrderByDescending(orderByExpr);
+                    else
+                        produtos = produtos.OrderBy(orderByExpr);
+                }
+                else
+                {
+                    produtos = produtos.OrderBy(x => x.Descricao);
+                }
+            }
+            else
+            {
+                produtos = produtos.OrderBy(x => x.Descricao);
+            }
+
+            // pagina a lista
+            int totalResultados = produtos.Count();
+            produtos = produtos.Skip(dataTableModel.start).Take(dataTableModel.length);
+
+            // monta o resultado final
+            List<object> result_data = new List<object>();
+            foreach (var produto in produtos)
+            {
+                List<object> result_item = new List<object> {
+                    produto.ProdutoId,
+                    produto.Descricao                  
+                };
+                result_data.Add(result_item);
+            }
+
+            return Json(new
+            {
+                recordsTotal = totalResultados,
+                recordsFiltered = totalResultados,
+                data = result_data
             });
         }
     }
